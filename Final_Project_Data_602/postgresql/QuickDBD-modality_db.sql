d331b5756ca371ca43264511e8d4064902fb3682
Mubashira Qari <81127634+uzmabb182@users.noreply.github.com>
2022-11-26 07:49:18 -0500

    datatypes is fixed for states_df

--- Final_Project_Data_602/postgresql/QuickDBD-modality_db.sql
+++ Final_Project_Data_602/postgresql/QuickDBD-modality_db.sql
@@ -6,63 +6,22 @@
 -- To reset the sample schema, replace everything with
 -- two dots ('..' - without quotes).
 
-CREATE TABLE "fips" (
-    "countyname" varchar   NOT NULL,
-    "fips" int   NOT NULL
+CREATE TABLE "states" (
+    "States" varchar   NOT NULL,
+    "Abbreviation" varchar   NOT NULL
 );
 
 CREATE TABLE "modality" (
-    "countyname" varchar   NOT NULL,
-    "fips" int   NOT NULL,
     "learning_modality" varchar   NOT NULL,
-    "state" varchar   NOT NULL,
-    "abbreviation" varchar   NOT NULL,
     "year" int   NOT NULL,
+    "state" varchar   NOT NULL,
     "student_count" int   NOT NULL
 );
 
 CREATE TABLE "covid" (
-    "countyname" varchar   NOT NULL,
-    "fips" int   NOT NULL,
     "state" varchar   NOT NULL,
-    "year" int   NOT NULL,
-    "cases_count" int   NOT NULL,
-    "deaths_count" int   NOT NULL
+    "cases" int   NOT NULL,
+    "deaths" int   NOT NULL,
+    "year" int   NOT NULL
 );
 
-SELECT * FROM "fips";
-
-SELECT * FROM covid_df;
-
-SELECT * FROM modality;
-
-SELECT * 
-FROM covid_df 
-WHERE "year" = 2021
-
-SELECT * 
-FROM modality 
-WHERE "year" = 2021
-
-
-
-SELECT * 
-FROM covid_df 
-WHERE year = 2021
-AND state = 'Alabama';
-
-SELECT state, SUM(cases_count) AS total_cases, SUM(deaths_count) AS total_deaths
-FROM covid_df
-WHERE year = 2021
-GROUP By state
-ORDER BY state
-
-SELECT
-   DISTINCT state
-FROM
-   covid_df;
-   
-SELECT
-   DISTINCT "countyname"
-FROM
-   covid_df;
